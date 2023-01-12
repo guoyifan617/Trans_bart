b3b5e5aba8b22d2e6d2957256d6a0aa31bb4e764
Hongfei Xu <hfxunlp@foxmail.com>
2023-01-12 18:06:06 +0800

    dbg utils.fmt.gec.noise.freader

--- utils/fmt/gec/noise/freader.py
+++ utils/fmt/gec/noise/freader.py
@@ -6,7 +6,21 @@ from utils.fmt.parser import parse_none
 
 from cnfg.hyp import cache_len_default
 
-class gec_noise_reader:
+def gec_noise_reader(fname=None, noiser=None, tokenizer=None, min_len=2, max_len=cache_len_default, print_func=print):
+
+	_s_max_len = max_len - 2
+	_fname = [fname] if isinstance(fname, str) else fname
+	for _f in _fname:
+		for tgt in line_reader(_f, keep_empty_line=False, print_func=print_func):
+			_l = len(tgt)
+			if (_l > min_len) and (_l < _s_max_len):
+				src = noiser(tgt)
+				for _s, _e, _t in generate_iter_data(tokenizer(src), tokenizer(tgt)):
+					_l = len(_s)
+					if _l < max_len:
+						yield _s, _e, _t
+
+class GECNoiseReader:
 
 	def __init__(self, fname, noiser, tokenizer, min_len=2, max_len=cache_len_default, print_func=print):
 
@@ -14,15 +28,4 @@ class gec_noise_reader:
 
 	def __call__(self, fname=None, noiser=None, tokenizer=None, min_len=None, max_len=None, print_func=print):
 
-		_fname, _noiser, _token, _min_len, _max_len = parse_none(fname, self.fname), parse_none(noiser, self.noiser), parse_none(tokenizer, self.tokenizer), parse_none(min_len, self.min_len), parse_none(max_len, self.max_len)
-		_s_max_len = _max_len - 2
-		_fname = [_fname] if isinstance(_fname, str) else _fname
-		for _f in _fname:
-			for tgt in line_reader(_f, keep_empty_line=False, print_func=parse_none(print_func, self.print_func)):
-				_l = len(tgt)
-				if (_l > _min_len) and (_l < _s_max_len):
-					src = _noiser(tgt)
-					for _s, _e, _t in generate_iter_data(_token(src), _token(tgt)):
-						_l = len(_s)
-						if _l < _max_len:
-							yield _s, _e, _t
+		return gec_noise_reader(fname=parse_none(fname, self.fname), noiser=parse_none(noiser, self.noiser), tokenizer=parse_none(tokenizer, self.tokenizer), min_len=parse_none(min_len, self.min_len), max_len=parse_none(max_len, self.max_len), print_func=parse_none(print_func, self.print_func))
