3582e676036bb79b862b863239309bcdd574df7d
Hongfei Xu <hfxunlp@foxmail.com>
2023-01-20 16:56:10 +0800

    fix transformer.PLM.NMT

--- transformer/PLM/NMT.py
+++ transformer/PLM/NMT.py
@@ -1,6 +1,7 @@
 #encoding: utf-8
 
 from transformer.NMT import NMT as NMTBase
+from utils.fmt.parser import parse_double_value_tuple, parse_none
 
 class NMT(NMTBase):
 
@@ -8,5 +9,7 @@ class NMT(NMTBase):
 
 		_model_name = parse_none(model_name, self.model_name)
 		enc_model_name, dec_model_name = parse_double_value_tuple(_model_name)
-		self.enc.load_plm(plm_parameters, model_name=enc_model_name, **kwargs)
-		self.dec.load_plm(plm_parameters, model_name=dec_model_name, **kwargs)
+		if hasattr(self, "enc") and hasattr(self.enc, "load_plm"):
+			self.enc.load_plm(plm_parameters, model_name=enc_model_name, **kwargs)
+		if hasattr(self, "dec") and hasattr(self.dec, "load_plm"):
+			self.dec.load_plm(plm_parameters, model_name=dec_model_name, **kwargs)
